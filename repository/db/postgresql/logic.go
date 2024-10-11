d839aa85c56559715e18b7d3586a859e062ee0aa
Neda Heshmatian <nidheshmatian@gmail.com>
2024-10-11 14:02:46 +1100

    add: implement database functions

--- repository/db/postgresql/logic.go
+++ repository/db/postgresql/logic.go
@@ -1,2 +1,87 @@
 package postgresql
 
+import (
+	"errors"
+	"time"
+
+	"github.com/nidoqueen1/article-api/entity"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+// CreateArticle stores a new article in database
+func (p *postgresql) CreateArticle(article *entity.Article) error {
+	tagNames := make([]string, len(article.Tags))
+	for i, tag := range article.Tags {
+		tagNames[i] = tag.Name
+	}
+
+	// insert new tags and return both new and existing tag IDs by updating existing ones
+	// raw query has been used because (*gorm.DB).Create returns ID only for the new entries by defaulf
+	var tagIDs []uint
+	query := `
+		INSERT INTO tags (name)
+		VALUES (?)
+		ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name 
+		RETURNING id`
+
+	for _, tagName := range tagNames {
+		var id uint
+		if err := p.db.Raw(query, tagName).Scan(&id).Error; err != nil {
+			return err
+		}
+		tagIDs = append(tagIDs, id)
+	}
+
+	// insert the article without tags first, clearing tags to avoid auto-insertion them
+	if err := p.db.Create(article).Error; err != nil {
+		return err
+	}
+
+	// then insert article_tag relationships
+	for _, tagID := range tagIDs {
+		if err := p.db.Exec("INSERT INTO article_tags (article_id, tag_id) VALUES (?, ?)", article.ID, tagID).Error; err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *postgresql) GetArticle(articleID uint) (*entity.Article, error) {
+	var article *entity.Article
+	err := p.db.Preload("Tags").First(&article, articleID).Error
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		err = nil
+		article = nil
+	}
+
+	return article, err
+}
+
+func (p *postgresql) GetArticlesByTagAndDate(tagName string, date time.Time) ([]*entity.Article, int64, error) {
+	var articles []*entity.Article
+	var totalCount int64
+
+	subquery := p.db.Model(&entity.Article{}).
+		Joins("JOIN article_tags ON articles.id = article_tags.article_id").
+		Joins("JOIN tags ON tags.id = article_tags.tag_id").
+		Where("tags.name = ?", tagName).
+		Where("articles.date = ?", date)
+
+	err := subquery.Count(&totalCount).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	err = subquery.Limit(viper.GetInt("list_articles.limit")).
+		Preload("Tags").
+		Order("articles.id DESC").
+		Find(&articles).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		err = nil
+	}
+
+	return articles, totalCount, err
+}
