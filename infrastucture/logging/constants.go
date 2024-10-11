c9065e1c5da3f216dbd8cd0069a8de77bfc7faca
Neda Heshmatian <nidheshmatian@gmail.com>
2024-10-11 13:43:09 +1100

    add: integrate logrus for logging

---
+++ infrastucture/logging/constants.go
@@ -0,0 +1,8 @@
+package logging
+
+const (
+	DebugMode = "debug"
+	InfoMode  = "info"
+	ErrorMode = "error"
+	FatalMode = "fatal"
+)
--- infrastucture/logging/init.go
+++ infrastucture/logging/init.go
@@ -1,2 +1,30 @@
 package logging
 
+import (
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func InitLog() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetLevel(getLogLevel())
+	logger.SetFormatter(&logrus.JSONFormatter{})
+	return logger
+}
+
+func getLogLevel() logrus.Level {
+	switch strings.ToLower(viper.GetString("log.level")) {
+	case DebugMode:
+		return logrus.DebugLevel
+	case InfoMode:
+		return logrus.InfoLevel
+	case ErrorMode:
+		return logrus.ErrorLevel
+	case FatalMode:
+		return logrus.FatalLevel
+	default:
+		return logrus.InfoLevel
+	}
+}
