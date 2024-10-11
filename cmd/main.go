8f1ce33ef644c6f315f077a7670e3b2ac7a99f63
Neda Heshmatian <nidheshmatian@gmail.com>
2024-10-11 14:07:56 +1100

    add: implement main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,15 @@
-package cmd
+package main
 
+import (
+	"log"
+
+	"github.com/nidoqueen1/article-api/app"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	server := app.New()
+	if err := server.Run(viper.GetString("server.addr")); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+}
