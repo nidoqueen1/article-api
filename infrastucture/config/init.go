6d2186cc95302ed69e1862d12802547ec940072d
Neda Heshmatian <nidheshmatian@gmail.com>
2024-10-11 13:40:47 +1100

    add: reading config from yaml

--- infrastucture/config/init.go
+++ infrastucture/config/init.go
@@ -1,2 +1,20 @@
 package config
 
+import (
+	"github.com/spf13/viper"
+)
+
+// InitConfig initializes the viper and loads the configuration.
+func InitConfig() error {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(".")
+	_ = viper.BindEnv("db.url", "DB_URL")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
