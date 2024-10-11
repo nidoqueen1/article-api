4892d448cc263205cd81e67a1c49d3ebdbe71428
Neda Heshmatian <nidheshmatian@gmail.com>
2024-10-11 13:46:24 +1100

    chore: add db factory

--- infrastucture/db/init.go
+++ infrastucture/db/init.go
@@ -1,2 +1,18 @@
 package db
 
+import (
+	"fmt"
+
+	"github.com/nidoqueen1/article-api/repository/db"
+	"github.com/nidoqueen1/article-api/repository/db/postgresql"
+	"github.com/sirupsen/logrus"
+)
+
+func InitDatabase(dbType string, l *logrus.Logger) (db.IDatabase, error) {
+	switch dbType {
+	case "postgresql":
+		return postgresql.InitDB(l)
+	default:
+		return nil, fmt.Errorf("unsupported database type: %s", dbType)
+	}
+}
